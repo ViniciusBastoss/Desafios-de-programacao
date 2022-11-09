e92f33efb2faf39af7ada2335f44d83c42b859b9
ViniciusBastoss <viniciusbuenozt@gmail.com>
2022-11-08 21:33:48 -0300

    Solução errada

--- Pedagio/Pedagio.cpp
+++ Pedagio/Pedagio.cpp
@@ -3,15 +3,22 @@
 #include <list>
 #include <iostream>
 #include <string.h>
+#include <string>
 //#include <iterator>
 using namespace std;
 
-struct carro
+/// @brief 
+typedef struct carro_
 {
     char placa[20];
     char horario[20];
     char status[5];
     int km;
+}carro;
+
+struct teste{
+    int a;
+    int b;
 };
 
 void printList(list<carro> lista)
@@ -55,24 +62,33 @@ void validaRegistros(list<carro> *registros)
     {
         if (it->status[1] == 'x' || (it->status[1] == 'n' && it2->status[1] == 'n'))
         {
-            printf("\n%s km:%d",it->placa, it->km);
+            //printf("\n%s km:%d",it->placa, it->km);
             registros->erase(it);
             it = it2;
             if (it2 != registros->end())
                 it2++;
-            printf("\nDB:Entrou");
+            //printf("\nDB:Entrou");
         }
         else
         {
+                
             if (it2 != registros->end())
             {
+                if(abs(strcmp(it->placa,it2->placa))){
+                    registros->erase(it);
+                    it = it2;
+                    if (it2 != registros->end())
+                    it2++;
+                }
+                else{
                 advance(it, 2);
                 advance(it2, 2);
+                }
             }
         }
         if(it2 == registros->end()){
             it2--;
-            printf("\n\nDebug Status:%c   Placa:%s\n\n",it2->status[1],it2->placa);
+            //printf("\n\nDebug Status:%c   Placa:%s\n\n",it2->status[1],it2->placa);
             if(it2->status[1] == 'n'){
                 registros->erase(it2);
                 it2 = registros->end();
@@ -80,15 +96,36 @@ void validaRegistros(list<carro> *registros)
 
         }
     }
-    printList(*registros);
+    //printList(*registros);
 }
 
+void calculaTarifa(list<carro> registros, int *valoresTarifa){
+    char indice[2],*placaAt;
+    float total = 2;
+    carro veiculo,veiculo2,aux;
+    while(registros.size() > 0){
+        aux = *registros.begin();
+        veiculo = *registros.begin();
+        veiculo2 = *next(registros.begin(),1);
+        while(!abs(strcmp(veiculo.placa,aux.placa))){
+           indice[0] = veiculo.horario[6];
+           indice[1] = veiculo.horario[7];
+           total +=  1 + (float)abs(veiculo2.km - veiculo.km) * ((valoresTarifa[atoi(indice)]/100.00)); 
+           registros.pop_front();
+           registros.pop_front();
+           veiculo = *registros.begin();
+           veiculo2 = *next(registros.begin(),1);
+        }
+        cout << aux.placa <<" "<< "$" << total << endl;
+        total = 2;
+    }
+}
 void processaRegistro(list<carro> *registros, int *tarifa)
 {
     
     list<carro>::iterator it = registros->begin();
     registros->sort(compara);
-    printList(*registros);
+    //printList(*registros);
     // it = registros->begin();
     // registros->erase(it);
     validaRegistros(registros);
@@ -99,7 +136,7 @@ int main()
 {
 
     // advance(it, 5); avança o iterator em 5 elementos
-/*
+
     int casos, tarifa[24];
     char linha[300] = "kkk";
     list<carro> registros;
@@ -117,26 +154,16 @@ int main()
         while (fgets(linha, 100, stdin))
         {
             if(strlen(linha) <= 1) break;
-            printf("\nLinha:%s",linha);
-            
+            //printf("\nLinha:%s",linha);
             sscanf(linha, "%s %s %s %d", dados.placa, dados.horario, dados.status, &dados.km);
-            printf("%d",dados.km);
             registros.push_back(dados);
-            // printf("\n%s\n",linha); //Debug
         }
-        registros.push_back(dados);
-        registros.pop_back();
-        //registros.erase(it);
-        //processaRegistro(&registros, tarifa);
-        //aux = *next(registros.end(),0);
-        it = next(registros.end(),-1);
-        advance(it,3);
-        printf("\nVazio placa:%d\n",it->km);//Debug
-        cout << endl;
-        printList(registros);
+        processaRegistro(&registros, tarifa);
+        calculaTarifa(registros,tarifa);
+        //printList(registros);
         registros.clear();
     }
-    */
+    
    /*
    list<carro> registro;
    char linha[100] = "dbqu 01:26:18:07 exit 687";
@@ -145,13 +172,17 @@ int main()
    registro.push_back(dados);
    registro.push_back(dados);
    registro.push_back(dados);
-   printf("\nPlaca:%s",registro.begin()->placa);
+   printf("\nPlaca:%s",registro.end()->placa);
    */
+   /*
   list<int> vet;
-  vet.push_back(1);
-  vet.push_back(2);
-  vet.push_back(3);
+  
+  vet.push_back(500);
+  vet.push_back(100);
+  vet.push_back(600);
 
-  printf("\nValor:%d",*vet.end());
+  //printf("\nValor:%d",vet.begin()->a);
+  cout << *next(vet.end(),-1);
+  */
 
 }
