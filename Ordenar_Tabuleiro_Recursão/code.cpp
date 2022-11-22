61ebef47a43550980a520193c28b21883dc65e3e
VViniciusB <viniciusouchia@gmail.com>
2022-11-22 12:00:09 -0300

    Declarando arvore e iniciando logica da recursão

--- Ordenar_Tabuleiro_Recursão/code.cpp
+++ Ordenar_Tabuleiro_Recursão/code.cpp
@@ -1,52 +1,74 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int cont = 0,vizinhos[4][2] = {0,1,
-                      0,-1,
-                      1,0,
-                      -1,0};
+struct direcao
+{
+    int x;
+    int y;
+};
 
-void printTab(int *vet){
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++)
-          printf("%d ",vet[4*i+j]);
+struct no_
+{
+    char direc;
+    //no_ *dir = NULL;
+    //no_ *esq = NULL;
+    //no_ *acima = NULL;
+    //no_ *baixo = NULL;
+    no_ *lados[4] = {NULL,NULL,NULL,NULL};
+} no;
+
+direcao vizinhos[4] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+char letras[4] = {'R', 'L', 'U', 'D'};
+
+void printTab(int *vet)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+            printf("%d ", vet[4 * i + j]);
         printf("\n");
-
     }
-
 }
 
-void resolve(int *tabela,int *posVazia){
-    for(int i = 0; i < 4;i++){
-            if(4*(posVazia[0]+ vizinhos[i][0])+ (posVazia[1] + vizinhos[i][1] < 16)){
-                buscaSol(tabela,posVazia,i);
-            }
+void resolve(int *posVazia, no_ *noAnterior,int cont)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (4 * (posVazia[0] + vizinhos[i].x) + (posVazia[1] + vizinhos[i].y < 16))
+        {
+            no_ *novo;
+            novo = (no_ *)calloc(1, sizeof(no_));
+            novo->direc = letras[i];
+            noAnterior->lados[i] = novo;
+            cont++;
 
-        
+            // buscaSol(tabela,posVazia,i);
+        }
     }
-       
-  
-
-    
-
 }
 
-int main(){
-    int tabela[16],tam,posVazia[2];
-    scanf("%d",&tam);
-    while(tam){
-        for(int i = 0; i < 4;i++){
-            for(int j = 0; j < 4;j++){
-                scanf("%d",&tabela[i*4+j]);
-                if(tabela[i*4+j] == 0)
-                posVazia[0] = i;
+int main()
+{
+    int tabela[16], tam, posVazia[2], cont = 0;
+    no_ *primeiroNo;
+    primeiroNo = (no_ *)calloc(1, sizeof(no_));
+    scanf("%d", &tam);
+    while (tam)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                scanf("%d", &tabela[i * 4 + j]);
+                if (tabela[i * 4 + j] == 0)
+                    posVazia[0] = i;
                 posVazia[1] = j;
             }
         }
-        resolve(tabela,posVazia);
+        resolve(posVazia,primeiroNo,cont);
         printTab(tabela);
         printf("\n");
         tam--;
     }
-
 }
